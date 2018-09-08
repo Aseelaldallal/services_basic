d7ea2e97e8fd8c35f39ec5d0e6e09efe6d535b5f
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-09-08 19:35:42 -0400

    create user service, move code to it

--- src/app/active-users/active-users.component.ts
+++ src/app/active-users/active-users.component.ts
@@ -1,15 +1,26 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../Services/users.service';
 
 @Component({
   selector: 'app-active-users',
   templateUrl: './active-users.component.html',
   styleUrls: ['./active-users.component.css']
 })
-export class ActiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToInactive = new EventEmitter<number>();
-
-  onSetToInactive(id: number) {
-    this.userSetToInactive.emit(id);
+export class ActiveUsersComponent implements OnInit {
+  
+  private users : string[] ;
+  private userService: UserService;
+  
+  constructor(userService: UserService) {
+    this.userService = userService;
   }
+
+  ngOnInit() {
+    this.users = this.userService.activeUsers;
+  }
+
+
+  // onSetToInactive(id: number) {
+  //   this.userSetToInactive.emit(id);
+  // }
 }
--- src/app/inactive-users/inactive-users.component.ts
+++ src/app/inactive-users/inactive-users.component.ts
@@ -1,15 +1,23 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../Services/users.service';
 
 @Component({
   selector: 'app-inactive-users',
   templateUrl: './inactive-users.component.html',
   styleUrls: ['./inactive-users.component.css']
 })
-export class InactiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToActive = new EventEmitter<number>();
 
-  onSetToActive(id: number) {
-    this.userSetToActive.emit(id);
+export class InactiveUsersComponent implements OnInit {
+
+  private users : string[];
+  private userSerivce: UserService;
+
+  constructor(userService: UserService) {
+    this.userSerivce = userService;
   }
+  
+ngOnInit() {
+  this.users = this.userSerivce.inactiveUsers;
+}
+
 }
