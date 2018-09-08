8a1b87ccb15feff3c35eff1955ac557a8c5255e1
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-09-08 19:47:47 -0400

    bug fix

--- src/app/Services/counter.service.ts
+++ src/app/Services/counter.service.ts
@@ -3,5 +3,10 @@
 
 export class CounterService {
 
+    private actions = 0;
 
+    incrementActions() {
+        this.actions++;
+        console.log('Total Number of Actions: ', this.actions);
+    }
 }
--- src/app/Services/users.service.ts
+++ src/app/Services/users.service.ts
@@ -1,18 +1,29 @@
+import { Injectable } from "@angular/core";
+import { CounterService } from "./counter.service";
 
 
+@Injectable()
 
 export class UserService {
-    activeUsers = ['Max', 'Anna'];
-    inactiveUsers = ['Chris', 'Manu'];
+
+    private activeUsers = ['Max', 'Anna'];
+    private inactiveUsers = ['Chris', 'Manu'];
+    private counterService: CounterService;
+
+    constructor(counterService: CounterService) {
+        this.counterService = counterService;
+    }
 
     setToInactive(id: number) {
         this.inactiveUsers.push(this.activeUsers[id]);
         this.activeUsers.splice(id, 1);
+        this.counterService.incrementActions();
     }
 
     setToActive(id: number) {
         this.activeUsers.push(this.inactiveUsers[id]);
         this.inactiveUsers.splice(id, 1);
+        this.counterService.incrementActions();
     }
 
 }
